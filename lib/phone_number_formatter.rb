55eb55267d1360f53095d8b2e797322f351f53b0
Chris Foral <chris.foral@gmail.com>
2016-01-25 14:44:28 -0600

    Prettier version

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -1,5 +1,9 @@
 def format_phone_number(phone_number_str)
   num_arr = phone_number_str.scan(/\d/)
 
-  "(" + num_arr[0..2].join + ")" + " " + num_arr[3..5].join + "-" + num_arr[6..-1].join
+  num_1 = num_arr[0..2].join
+  num_2 = num_arr[3..5].join
+  num_3 = num_arr[6..-1].join
+
+  "(" + num_1 + ") " + num_2 + "-" + num_3
 end
